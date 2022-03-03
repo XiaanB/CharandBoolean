70ab41c5b114c2439a038dcc078da7c186a20de4
xiaan <xiaan_b@hotmail.com>
2022-03-04 10:22:14 +1300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+    }
+}
